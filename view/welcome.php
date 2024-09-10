1e9e4d530ae00621d01f075a0d1544fcad4fc60f
Braindance <48202226+penginman@users.noreply.github.com>
2024-09-10 09:22:13 +0800

    Update welcome.php

--- view/welcome.php
+++ view/welcome.php
@@ -194,7 +194,7 @@
 </div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
